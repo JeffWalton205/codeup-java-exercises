73cafa8eb7bdcd72d7a4f849381a945d972e510f
Jeff Walton <jeff.walton205@gmail.com>
2020-04-23 09:01:45 -0500

    exercises

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,38 +1,69 @@
-package grades;
-
-import java.util.ArrayList;
-
-public class Student {
-
-    private String name;
-    private ArrayList<Integer> grades;
-
-    public Student (String name) {
-        this.name = name;
-        this.grades = new ArrayList<>();
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void addGrade(int grade) {
-        this.grades.add(grade);
-    }
-
-    public float getAverageGrade() {
-        float newTotal = 0;
-        for (float grade : this.grades) {
-            newTotal += grade;
-        } return newTotal / this.grades.size();
-    }
-
-    public static void main(String[] args) {
-        Student jeff = new Student("jeff");
-        jeff.addGrade(70);
-        jeff.addGrade(70);
-        jeff.addGrade(90);
-        System.out.println(jeff.getAverageGrade());
-    }
-
-}
+//package grades;
+//
+//import java.util.ArrayList;
+//
+////public class Student {
+////
+////    private String name;
+////    private ArrayList<Integer> grades;
+////
+////    public Student (String name) {
+////        this.name = name;
+////        this.grades = new ArrayList<>();
+////    }
+////
+////    public String getName() {
+////        return name;
+////    }
+////
+////    public void addGrade(int grade) {
+////        this.grades.add(grade);
+////    }
+////
+////    public float getAverageGrade() {
+////        float newTotal = 0;
+////        for (float grade : this.grades) {
+////            newTotal += grade;
+////        } return newTotal / this.grades.size();
+////    }
+////
+////    public static void main(String[] args) {
+////        Student jeff = new Student("jeff");
+////        jeff.addGrade(70);
+////        jeff.addGrade(70);
+////        jeff.addGrade(90);
+////        System.out.println(jeff.getAverageGrade());
+////    }
+//
+//
+//
+//
+//public interface Marker {
+//}
+//
+////
+////abstract class Marker {
+////    public void write();
+////}
+////
+////
+////public class Marker {
+////    public void write() {}
+////}
+////
+////
+////interface Marker {
+////    void write();
+////}
+////
+////
+////interface Marker {
+////    public void write();
+////}
+////
+////
+////interface Marker {
+////}
+//
+//
+//}
